2c97b0c8be7bb3ca84a084cfd993361d70aa7ba2
AndreyWeber <andrey.weber@gmail.com>
2024-04-28 01:23:06 +0100

    Added Qt application main window stub. Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+pip install PyQt5
+pip install PyQt5-stubs
+pip install lxml
--- main1.py
+++ main1.py
@@ -1,7 +1,11 @@
 import sys
 from pathlib import Path
+
+# pylint: disable=no-name-in-module
+from PyQt5.QtWidgets import QApplication
 from dry_cleaning import DryCleaning
 from xml_data_handler import XmlDataHandler
+from main_window import MainWindow
 
 try:
     xml_data_handler = XmlDataHandler(
@@ -10,7 +14,12 @@ try:
         str(Path(".\\newfile.xml").resolve()),
     )
     xml_data_handler.read()
-    xml_data_handler.write()
+
+    app = QApplication(sys.argv)
+    main_window = MainWindow(xml_data_handler.data_source)
+    main_window.show()
+    sys.exit(app.exec_())
+    # xml_data_handler.write()
 except Exception as e:
     print(f"An error occurred: {e}")
     sys.exit()
